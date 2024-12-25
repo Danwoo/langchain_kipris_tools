940cb766bddd25a319fc0ccad28fb1712a220cde
jaeho kang <greennuri@gmail.com>
2024-12-25 18:10:03 +0000

    해외특허   예제추가   자유검색 api tool 추가

--- langchain_kipris_tools/__init__.py
+++ langchain_kipris_tools/__init__.py
@@ -1,14 +1,30 @@
 __version__ = '0.0.2'
 from langchain_kipris_tools.kipris_tools import(
-    ApplicantSearchTool, PatentKeywordSearchTool, PatentSearchTool, ApplicationNumberSearchTool, RighterSearchTool
+    KoreanApplicantSearchTool, 
+    KoreanPatentKeywordSearchTool, 
+    KoreanPatentSearchTool, 
+    KoreanRighterSearchTool, 
+    KoreanApplicationNumberSearchTool,
+    ForeignPatentSearchTool
     ) 
+
+
 import os
 import typing as t
 from langchain_core.tools import BaseTool
+
 class LangChainKiprisKoreanTools:
     def set_api_key(self, api_key: str):
         self.api_key = api_key
         os.environ['KIPRIS_API_KEY'] = api_key
     def get_tools(self) -> t.List[BaseTool]:
-        tool_list = [ApplicantSearchTool(), PatentKeywordSearchTool(), PatentSearchTool(), ApplicationNumberSearchTool(), RighterSearchTool()]
+        tool_list = [KoreanApplicantSearchTool(), KoreanPatentKeywordSearchTool(), KoreanPatentSearchTool(), KoreanRighterSearchTool(), KoreanApplicationNumberSearchTool()]
+        return tool_list
+
+class LangChainKiprisForeignTools:
+    def set_api_key(self, api_key: str):
+        self.api_key = api_key
+        os.environ['KIPRIS_API_KEY'] = api_key
+    def get_tools(self) -> t.List[BaseTool]:
+        tool_list = [ForeignPatentSearchTool()]
         return tool_list
--- langchain_kipris_tools/kipris_tools/__init__.py
+++ langchain_kipris_tools/kipris_tools/__init__.py
@@ -1,14 +1,17 @@
-from langchain_kipris_tools.kipris_tools.korean.applicant_search_tool import ApplicantSearchTool
-from langchain_kipris_tools.kipris_tools.korean.patent_keyword_search_tool import PatentKeywordSearchTool
-from langchain_kipris_tools.kipris_tools.korean.patent_search_tool import PatentSearchTool
-from langchain_kipris_tools.kipris_tools.korean.righter_search_tool import RighterSearchTool
-from langchain_kipris_tools.kipris_tools.korean.application_number_search_tool import ApplicationNumberSearchTool
+from langchain_kipris_tools.kipris_tools.korean.applicant_search_tool import ApplicantSearchTool as KoreanApplicantSearchTool
+from langchain_kipris_tools.kipris_tools.korean.patent_keyword_search_tool import PatentKeywordSearchTool as KoreanPatentKeywordSearchTool
+from langchain_kipris_tools.kipris_tools.korean.patent_search_tool import PatentSearchTool as KoreanPatentSearchTool
+from langchain_kipris_tools.kipris_tools.korean.righter_search_tool import RighterSearchTool as KoreanRighterSearchTool
+from langchain_kipris_tools.kipris_tools.korean.application_number_search_tool import ApplicationNumberSearchTool as KoreanApplicationNumberSearchTool
+
+from langchain_kipris_tools.kipris_tools.foreign.patent_search_tool import ForeignPatentSearchTool as ForeignPatentSearchTool
 
 __all__ = [
-    "ApplicantSearchTool",
-    "PatentKeywordSearchTool",
-    "PatentSearchTool",
-    "RighterSearchTool",
-    "ApplicationNumberSearchTool"
+    "KoreanApplicantSearchTool",
+    "KoreanPatentKeywordSearchTool",
+    "KoreanPatentSearchTool",
+    "KoreanRighterSearchTool",
+    "KoreanApplicationNumberSearchTool",
+    "ForeignPatentSearchTool"
 ]
 
