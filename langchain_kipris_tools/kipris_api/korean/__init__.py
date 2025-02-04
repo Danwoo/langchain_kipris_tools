8ce5c840c052615139537f51b1db004286f4fe44
jaeho kang <greennuri@gmail.com>
2025-02-04 12:38:01 +0000

    특허 상세 검색 api 추가

--- langchain_kipris_tools/kipris_api/korean/__init__.py
+++ langchain_kipris_tools/kipris_api/korean/__init__.py
@@ -3,10 +3,12 @@ from langchain_kipris_tools.kipris_api.korean.application_number_search_api impo
 from langchain_kipris_tools.kipris_api.korean.patent_search_api import PatentSearchAPI
 from langchain_kipris_tools.kipris_api.korean.free_search_api import PatentFreeSearchAPI
 from langchain_kipris_tools.kipris_api.korean.righter_search_api import PatentRighterSearchAPI
+from langchain_kipris_tools.kipris_api.korean.patent_detail_search_api import PatentDetailSearchAPI
 __all__ = [
     "PatentApplicationNumberSearchAPI",
     "PatentApplicantSearchAPI",
     "PatentFreeSearchAPI",
     "PatentSearchAPI",
-    "PatentRighterSearchAPI"
+    "PatentRighterSearchAPI",
+    "PatentDetailSearchAPI"
 ]
---
+++ langchain_kipris_tools/kipris_tools/korean/patent_detail_search_tool.py
@@ -0,0 +1,22 @@
+from langchain_kipris_tools.kipris_api.korean import PatentDetailSearchAPI
+from langchain_core.tools import BaseTool
+from pydantic import BaseModel, Field
+import typing as t
+import pandas as pd
+
+class PatentDetailSearchArgs(BaseModel):
+    application_number: str = Field("", description="Application number")    
+
+class PatentDetailSearchTool(BaseTool):
+    name:str = "korean_patent_detail_search"
+    description:str = "patent detail search"
+    api:PatentDetailSearchAPI = PatentDetailSearchAPI()
+    args_schema:t.Type[BaseModel] = PatentDetailSearchArgs
+    return_direct: bool = False
+
+    def _run(self, application_number:str)->pd.DataFrame:
+        if not application_number:
+            raise ValueError("you must provide application_number")
+            
+        result = self.api.search(application_number=application_number)
+        return result
