3a8fd731d64ebb3fac1d732fec1f4428c158d7f4
Your Name <your-email@example.com>
2025-07-30 00:45:40 +0900

    free_search_api에서 applicant_number 컬럼명 수정

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
+import os
 from setuptools import setup, find_packages
 
 setup(
     name="langchain_kipris_tools",  # 패키지 이름
-    version="0.0.2",           # 패키지 버전
+    version="0.0.2",  # 패키지 버전
     author="jaeho. kang",
     author_email="greennuri@gmail.com",
     description="kipris api tools for langchain",
-    long_description=open("README.md").read(),
+    long_description=(
+        open("README.md", encoding="utf-8").read()
+        if os.path.exists("README.md")
+        else ""
+    ),
     long_description_content_type="text/markdown",
     url="https://github.com/nuri428/langchain_kipris_tools",  # 패키지 URL (옵션)
     packages=find_packages(),
@@ -15,8 +20,8 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.9",      # Python 버전 요구 사항
-    install_requires=[            # 의존 패키지
+    python_requires=">=3.9",  # Python 버전 요구 사항
+    install_requires=[  # 의존 패키지
         "langchain",
         "langchain_core",
         "langgraph",
